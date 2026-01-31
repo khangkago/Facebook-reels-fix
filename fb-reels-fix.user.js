2bc2aa62110cf1bab9dd415c8e3cee1c36fab1b4
khangkago <khangkago25@gmail.com>
2026-01-31 14:25:04 +0700

    Update fb-reels-fix.user.js

--- fb-reels-fix.user.js
+++ fb-reels-fix.user.js
@@ -1,17 +1,34 @@
 // ==UserScript==
 // @name         Fix Facebook Reels Auto-Pause
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      2.0
 // @description  Fix Auto-Pause
 // @author       khangkago
 // @match        https://www.facebook.com/*
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function() {
     'use strict';
-    Object.defineProperty(document, 'visibilityState', { value: 'visible', writable: false });
-    Object.defineProperty(document, 'hidden', { value: false, writable: false });
-    window.addEventListener('visibilitychange', (e) => e.stopImmediatePropagation(), true);
-    window.addEventListener('blur', (e) => e.stopImmediatePropagation(), true);
+    Object.defineProperties(document, {
+        'visibilityState': {
+            get: function() { return 'visible'; }
+        },
+        'hidden': {
+            get: function() { return false; }
+        }
+    });
+    if (!Document.prototype._originalHasFocus) {
+        Document.prototype._originalHasFocus = Document.prototype.hasFocus;
+        Document.prototype.hasFocus = function() { return true; };
+    }
+    const blockEvents = ['visibilitychange', 'webkitvisibilitychange', 'blur', 'focusout', 'pagehide'];
+
+    blockEvents.forEach(eventName => {
+        window.addEventListener(eventName, (e) => {
+            e.stopImmediatePropagation();
+            e.stopPropagation();
+        }, true);
+    });
 })();
